35ac5b0a0a6b23701566be8faa5407333e758847
LeoPorcellatti <leoporcellatti@live.com.ar>
2023-10-08 20:40:42 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ParcialSysacad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
